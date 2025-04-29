854c3bb41b1cf9824e58e39a96e613824f8202a5
NicatHsynlyv <nicat200507@gmail.com>
2025-04-29 11:46:44 +0400

    Buraya etdiyiniz dəyişikliklərlə bağlı izah yazın

---
+++ src/test/java/com/example/serverexample8/Serverexample8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverexample8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serverexample8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
